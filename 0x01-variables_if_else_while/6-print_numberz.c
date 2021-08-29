002309a7b695a59420365e5956d74efaf49eaabc
Oniy16 <yonathanashenafi@gmail.com>
2021-08-29 06:48:20 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - the fun that print number using putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(n);
+	putchar('\n')
+	return (0);
+}
