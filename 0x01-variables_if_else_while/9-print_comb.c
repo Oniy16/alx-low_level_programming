71e42b65699fd668265973237c984949fa513814
Oniy16 <yonathanashenafi@gmail.com>
2021-08-29 20:06:10 -0700

    print no with comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,11 @@ int main(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		putchar(n);
+		putchar(n % 10 + '0');
 		if (n != '9')
 		{
 			putchar(',');
-			putchar('');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
