0540acf9a23ffb8e15d9dca8ef735e913334aaf9
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-01 13:16:31 -0700

    advance times_table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Check holberton
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: Nothing.
+ */
+void print_times_table(int n)
+{
+	int value, i, j, k;
+
+	if (n < 0 || n > 15)
+	{
+		value = 1;
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			k = i * j;
+
+			if ((k / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(n + '0');
+				if (j == n)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+				if (j == n)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+		value = 0;
+	}
+	return (value);
+}
