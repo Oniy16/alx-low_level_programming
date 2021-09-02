dfb95400a36b4fe02c54c6c65afa7b890e9bcf9a
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-02 13:36:22 -0700

    largest no

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,7 +10,12 @@
 
 int largest_number(int a, int b, int c)
 {
+	int a, b, c;
 	int largest;
+
+	a = 972;
+        b = -98;
+        c = 0;
 
 	if (a > b && a > c)
 	{
@@ -24,5 +29,7 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
 	return (largest);
 }
