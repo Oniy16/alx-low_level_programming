f720d8c41a6998aca1b94af26edbcc5431b45830
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-02 21:14:00 -0700

    isupper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	if (c < 97)
+		printf("%c: %d\n", c, _isupper(c));
+	else
+		printf("%c: %d\n", c, _isupper(c));
+	retrn(0);
+}
