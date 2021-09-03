da56e276913ffb1ee912a40d408a4608bf02d366
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-03 09:48:50 -0700

    is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _isdigit - Check if a character is a digit
+  * @x: The number to be checked
+  *
+  * Return: 1 for a character that will be a digit or 0 for any else
+  */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	return (0);
+}
