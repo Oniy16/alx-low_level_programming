6802c5f60fe9a16ff6a4c774f9f84304726cfae9
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-03 13:00:40 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,9 +4,10 @@
  * mul - Check if a character is a digit
  * @a: The 1st number to be mul
  * @b: The 2nd number to be mul
- * Return: 0 for any correct one
+ *
+ * Return: for any correct one
  */
 int mul(int a, int b)
 {
-	return (a*b);
+	return (a * b);
 }
