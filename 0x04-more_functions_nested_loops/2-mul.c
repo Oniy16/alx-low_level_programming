75d3c88e36ecb9badfb83adf17f59c67980e33d5
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-03 10:09:42 -0700

    is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-  * _isdigit - Check if a character is a digit
-  * @a: The 1st number to be mul
-  * @b: The 2nd number to be mul
-  * Return: 0 for any correct one
-  */
+ * _mul - Check if a character is a digit
+ * @a: The 1st number to be mul
+ * @b: The 2nd number to be mul
+ * Return: 0 for any correct one
+ */
 int mul(int a, int b)
 {
 	int x;
