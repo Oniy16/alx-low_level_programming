620128bc8cfaa234441e2a822a58276af4523a22
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-03 12:57:37 -0700

    mul

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,15 +1,12 @@
 #include "main.h"
 
 /**
- * _mul - Check if a character is a digit
+ * mul - Check if a character is a digit
  * @a: The 1st number to be mul
  * @b: The 2nd number to be mul
  * Return: 0 for any correct one
  */
 int mul(int a, int b)
 {
-	int x;
-
-	x = a * b;
-	return (x);
+	return (a*b);
 }
