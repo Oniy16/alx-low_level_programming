02eff01aa7b991ab2de32e36e5b3c9b8c70f821a
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-03 10:23:05 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Check if a character is a digit
+ * Return: 0 for a character that will be a digit or 0 for any else
+ */
+void print_numbers(void)
+{
+	int x;
+
+	for (x=0; x<10; x++)
+		_putchar(x);
+	_putchar('\n');
+	return (0);
+}
