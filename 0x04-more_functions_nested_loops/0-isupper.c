f1476d27500d450ae4cc95ad6eba00a2f79adaa9
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-03 09:29:18 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _isupper - check the code.
  * @x: The number to be check
@@ -8,8 +7,6 @@
 int _isupper(int x)
 {
 	if (x >= 65 && x <= 90)
-	{
 		return(1);
-	}
-		return (0);
+	return (0);
 }
