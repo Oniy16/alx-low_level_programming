94cbe894d72f9be21f737fcbd760a050048e8de9
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-06 12:05:14 -0700

    reset_to_98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A pointer that print int and update value.
+ * @n: The number of _ characters to be printed.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
