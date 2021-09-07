ef8076e5c3bd1a226fc735c584c336c72017b583
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-09-07 10:58:31 -0700

    rev string

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <stdlib.h>
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -9,4 +11,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
-char _strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
+
+#endif
