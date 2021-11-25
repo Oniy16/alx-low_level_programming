48026731f2a0bacc92b2bac0429457fba404e2e7
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2021-11-25 13:47:46 -0800

    using while loop

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,12 +8,15 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	const dlistint_t *temp;
+	size_t count = 0;
 
-	for (count = 0; h != NULL; count++)
+	temp = h;
+	while (temp != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
 	}
 	return (count);
 }
