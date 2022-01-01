2b7a22169c315f98b335609f479c22ffcd76e710
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2022-01-01 15:06:05 -0800

    fix issue with betty code style

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int i, sum = 0;
 
-	for( i = 0; i < 1024; i++)
+	for(i = 0; i < 1024; i++)
 	{
-		if( i % 3 == 0  || i % 5 == 0)
+		if(i % 3 == 0  || i % 5 == 0)
 			sum += i;
 	}
-	printf( "%d\n", sum);
-	return(0);
+	printf("%d\n", sum);
+	return (0);
 }
