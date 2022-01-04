edc6d894487e71b473e14df2b2f67c8c18ee0b08
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2022-01-03 17:22:58 -0800

    fix betty code style

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -10,11 +10,11 @@ void print_times_table(int n)
 {
 	int x, y;
 
-	if (n <= 15 && n >= 0  )
+	if (n <= 15 && n >= 0)
 	{
-		for (x = 0; x <= n; x++ )
+		for (x = 0; x <= n; x++)
 		{
-			for (y = 0; y <= n; y++ )
+			for (y = 0; y <= n; y++)
 			{
 				if( x * y < 10)
 				{
@@ -26,17 +26,17 @@ void print_times_table(int n)
 				}
 				else if (x * y > 99)
 				{
-					_putchar( ((x * y) / 100) + '0');
-					_putchar( (((x * y) / 10) % 10) + '0');
-					_putchar( ((x * y) % 10) + '0');
+					_putchar(((x * y) / 100) + '0');
+					_putchar((((x * y) / 10) % 10) + '0');
+					_putchar(((x * y) % 10) + '0');
 					_putchar(',');
 					_putchar(' ');
 				}
 				else
 				{
 					_putchar(' ');
-					_putchar( ((x * y) / 10) + '0');
-					_putchar( ((x * y) % 10) + '0');
+					_putchar(((x * y) / 10) + '0');
+					_putchar(((x * y) % 10) + '0');
 					_putchar(',');
 					_putchar(' ');
 				}
