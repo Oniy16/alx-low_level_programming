301d02421f4aaaf9a432893bb0298767a65deeaf
Yonatan Ashenafi <yonathanashenafi@gmail.com>
2022-01-03 17:20:28 -0800

    restructure code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * print_alphabet_x10 - Check holberton
  * Description: function uses _putchar function to print
@@ -7,40 +8,40 @@
  */
 void print_times_table(int n)
 {
-    int x, y;
+	int x, y;
 
-    if (n <= 15 && n >= 0  )
-    {
-        for (x = 0; x <= n; x++ )
-        {
-            for (y = 0; y <= n; y++ )
-            {
-                if( x * y < 10)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                    _putchar( x * y + '0');
-                    _putchar(',');
-                    _putchar(' ');
-                }
-                else if (x * y > 99)
-                {
-                    _putchar( ((x * y) / 100) + '0');
-                    _putchar( (((x * y) / 10) % 10) + '0');
-                    _putchar( ((x * y) % 10) + '0');
-                    _putchar(',');
-                   _putchar(' ');
-                }
-                else
-                {
-                    _putchar(' ');
-                    _putchar( ((x * y) / 10) + '0');
-                    _putchar( ((x * y) % 10) + '0');
-                    _putchar(',');
-                    _putchar(' ');
-                }
-            }
-	    _putchar('\n');
-        }
-    }
+	if (n <= 15 && n >= 0  )
+	{
+		for (x = 0; x <= n; x++ )
+		{
+			for (y = 0; y <= n; y++ )
+			{
+				if( x * y < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar( x * y + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else if (x * y > 99)
+				{
+					_putchar( ((x * y) / 100) + '0');
+					_putchar( (((x * y) / 10) % 10) + '0');
+					_putchar( ((x * y) % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar( ((x * y) / 10) + '0');
+					_putchar( ((x * y) % 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
